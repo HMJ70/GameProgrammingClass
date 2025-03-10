57721369bc089d587c716a36d6e574241bc01e70
HMJ70 <hans.melvern1233@gmail.com>
2025-03-10 22:27:27 +0700

    PLAYER IDLE RUN

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,9 +6,12 @@ public class Movement : MonoBehaviour
     private Rigidbody2D rb2d;
     private Vector2 direction = new Vector2();
 
+    Animator animator;
+
     private void Awake()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();    
     }
 
     public void SetDirection(float xDirection)
@@ -18,4 +21,8 @@ public class Movement : MonoBehaviour
         rb2d.velocity = direction;
     }
 
+    private void FixedUpdate()
+    {
+        animator.SetFloat("xVelocity", System.Math.Abs(rb2d.velocity.x));
+    }
 }
