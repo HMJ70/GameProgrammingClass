af8ebfd12a53918210714bed89bb3a9c6853a700
HMJ70 <hans.melvern1233@gmail.com>
2025-06-01 03:10:07 +0700

    ShootCooldown

--- Assets/Scripts/shoot.cs
+++ Assets/Scripts/shoot.cs
@@ -6,23 +6,28 @@ public class shoot : MonoBehaviour
 {
     public GameObject bulletprefab;
     public float bulletspeed = 50f;
+    public float shootCooldown = 1f;
 
+    private float lastShootTime;
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && Time.time >= lastShootTime + shootCooldown)
         {
             Shooting();
+            lastShootTime = Time.time;
         }
     }
 
     void Shooting()
     {
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = 0f; 
+
         Vector3 shootdirection = (mousePosition - transform.position).normalized;
 
         GameObject bullet = Instantiate(bulletprefab, transform.position, Quaternion.identity);
-        bullet.GetComponent<Rigidbody2D>().velocity = new Vector2(shootdirection.x, shootdirection.y) * bulletspeed;
+        bullet.GetComponent<Rigidbody2D>().velocity = shootdirection * bulletspeed;
         Destroy(bullet, 2f);
     }
 }
